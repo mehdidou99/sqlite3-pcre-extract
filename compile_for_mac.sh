1ca49908903cfe7397e139f9f079b1255a8e8cb2
Mehdi Benharrats <mehdi.benharrats@polytechnique.edu>
2022-06-28 14:30:20 +0200

    Correct mac compilation

--- compile_for_mac.sh
+++ compile_for_mac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cc -shared -o pcre.so -I/usr/local/opt/pcre/include  -fPIC -W -Werror pcre.c -L/usr/local/opt/pcre/lib -lpcre
-cc -shared -o pcre.so -I/usr/local/opt/pcre/include  -fPIC -W -Werror pcre_extract.c -L/usr/local/opt/pcre/lib -lpcre
+cc -shared -o pcre_extract.so -I/usr/local/opt/pcre/include  -fPIC -W -Werror pcre_extract.c -L/usr/local/opt/pcre/lib -lpcre
